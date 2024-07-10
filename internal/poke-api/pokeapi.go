c4c9fcc980f58a44c6622e2d01efc58e16e8ef71
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-07-10 16:29:50 -0400

    location struct

---
+++ internal/poke-api/pokeapi.go
@@ -0,0 +1,13 @@
+// module to interact with external poke api
+
+package pokeapi
+
+type LocationAreResp struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
